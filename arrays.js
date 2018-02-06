56ae5780a7c02f61f3dba7da3ffd70369270af50
Aurelien Xavier Payong D <xavier.payong@gmail.com>
2018-02-06 22:52:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"]
 
 function addElementToBeginningOfArray(array, element){
+  var element = "foo"
   array.unshift(element)
 }
 
